b90c7cfba3604f654824f36d385d9f4eb31b0f4e
adamsco <sco3.1415@gmail.com>
2015-04-11 11:29:37 +0200

    added sidecrollershooter

---
+++ SideScrollerShooter/Assets/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void FixedUpdate(){
+		if(Input.GetKey ("w"){
+			gameObject.GetComponent<Rigidbody>().AddForce(0.0f, speed, 0.0f);
+		}
+		if(Input.GetKey ("s"){
+			gameObject.GetComponent<Rigidbody>().AddForce(0.0f, -speed, 0.0f);
+			
+		}
+	}
+}
